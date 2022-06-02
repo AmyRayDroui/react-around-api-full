d55814744be2f0505ce5fc57a5f75899b1efa20b
AmyRayDroui <amyraydroui@gmail.com>
2022-06-02 15:07:58 +0300

    Add crush test

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,11 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
